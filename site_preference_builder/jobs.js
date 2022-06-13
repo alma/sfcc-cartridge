903fa39f609087baa4dc5381f9062a5cc3c85a20
Paul Roussel <paul.roussel-ext@getalma.eu>
2022-06-13 11:00:01 +0200

    changed require to fix async issue

--- site_preference_builder/jobs.js
+++ site_preference_builder/jobs.js
@@ -1,5 +1,5 @@
 const { readFileSync, writeFileSync } = require('fs');
-const { xmlToJson, jsonToXML } = require('./customSitePrefBuilder');
+const builder = require('./customSitePrefBuilder');
 
 const INPUT_JOB_SHIPMENT_FILE = './site_preference_builder/ref/jobs/jobShipment.xml';
 const INPUT_JOB_REFUND_FILE = './site_preference_builder/ref/jobs/jobRefund.xml';
@@ -16,34 +16,34 @@ const merchantHasOnShipment = (plans) => {
 exports.merchantHasOnShipment = merchantHasOnShipment;
 
 const getJobShipment = async (plans, siteName) => {
-  // const { xmlToJson } = require('./customSitePrefBuilder.js');
+  // const { builder.xmlToJson } = require('./customSitePrefBuilder.js');
 
   if (merchantHasOnShipment(plans)) {
     const jobShipment = readFileSync(INPUT_JOB_SHIPMENT_FILE).toString();
-    const onShipmentJob = await xmlToJson(jobShipment.replace('[[SITENAME]]', siteName));
+    const onShipmentJob = await builder.xmlToJson(jobShipment.replace('[[SITENAME]]', siteName));
     allJobs.job.push(onShipmentJob.job);
   }
 };
 
 const getJobRefund = async (toggleRefund, siteName) => {
-  // const { xmlToJson } = require('./customSitePrefBuilder.js');
+  // const { builder.xmlToJson } = require('./customSitePrefBuilder.js');
 
   if (toggleRefund === 'on') {
     const jobRefund = readFileSync(INPUT_JOB_REFUND_FILE).toString();
-    const onRefundJob = await xmlToJson(jobRefund.replace('[[SITENAME]]', siteName));
+    const onRefundJob = await builder.xmlToJson(jobRefund.replace('[[SITENAME]]', siteName));
     allJobs.job.push(onRefundJob.job);
   }
 };
 
 exports.writeJobsFile = async (toggleRefund, plans, siteName) => {
-  // const { xmlToJson, jsonToXML } = require('./customSitePrefBuilder.js');
+  // const { builder.xmlToJson, builder.jsonToXML } = require('./customSitePrefBuilder.js');
 
   await getJobShipment(plans, siteName);
   await getJobRefund(toggleRefund, siteName);
 
   const jobs = readFileSync(INPUT_JOBS_FILE);
-  const jobsContent = await xmlToJson(jobs);
+  const jobsContent = await builder.xmlToJson(jobs);
 
   jobsContent.jobs = Object.assign(jobsContent.jobs, allJobs);
-  writeFileSync(OUTPUT_JOB_SHIPMENT_FILE, jsonToXML(jobsContent));
+  writeFileSync(OUTPUT_JOB_SHIPMENT_FILE, builder.jsonToXML(jobsContent));
 };
