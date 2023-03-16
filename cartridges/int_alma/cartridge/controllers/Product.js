ae75e02d642c10d3b1c37d9ae93a771011c3a07b
joyet-simon <simon.joyet@getalma.eu>
2023-03-16 14:36:49 +0100

    feat: INT-1608 category exclusion in product page

--- cartridges/int_alma/cartridge/controllers/Product.js
+++ cartridges/int_alma/cartridge/controllers/Product.js
@@ -19,16 +19,20 @@ server.append('Show', function (req, res, next) {
 server.get('ProductWidgetData', server.middleware.https, function (req, res, next) {
     var ProductFactory = require('*/cartridge/scripts/factories/product');
     var almaPlanHelper = require('*/cartridge/scripts/helpers/almaPlanHelper');
+    var almaHelpers = require('*/cartridge/scripts/helpers/almaHelpers');
 
     var params = {
         method: 'GET',
         pid: req.querystring.pid
     };
     var product = ProductFactory.get(params);
+
+    var isWidgetEnabled = almaHelpers.haveExcludedCategory([req.querystring.pid]) ? false : almaWidgetHelper.isWidgetEnabled('product');
+
     var productQte = req.querystring.qte;
     var amount = Math.round(product.price.sales.value * productQte * 100);
     res.json({
-        isWidgetEnabled: almaWidgetHelper.isWidgetEnabled('product'),
+        isWidgetEnabled: isWidgetEnabled,
         paymentTypes: almaPlanHelper.getPlansForWidget(),
         amount: amount,
         product: product
