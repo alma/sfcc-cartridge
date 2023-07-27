c0a76b0a345f8a59d1e790affe1dbe8227850c50
Francois-Gomis <francois.gomis@getalma.eu>
2023-07-27 17:37:15 +0200

    test : Update sinon package and test button getter

--- cartridges/int_alma/cartridge/scripts/helpers/almaConfigHelper.js
+++ cartridges/int_alma/cartridge/scripts/helpers/almaConfigHelper.js
@@ -1,8 +1,12 @@
+var Site = require('dw/system/Site');
+
 /**
  * return the deferred capture activation field value
- */
+ * @return {bool} the field value
+ *
+*/
 function isDeferredCaptureEnable() {
-
+    return Site.getCurrent().getCustomPreferenceValue('ALMA_Deferred_Capture_Activation');
 }
 
 module.exports = {
--- test/unit/int_alma/scripts/helpers/almaConfigHelperTest.js
+++ test/unit/int_alma/scripts/helpers/almaConfigHelperTest.js
@@ -1,15 +1,21 @@
-// // almaConfigHelper.js unit tests
-//
-//
-// var assert = require('chai').assert;
-//
-// var almaConfigHelper = require('../../../../../cartridges/int_alma/cartridge/scripts/helpers/almaConfigHelper');
-// var
-//
-// // describe('Get back-office variables', function () {
-// //     describe('Deferred Capture', function () {
-// //         it('should call site preferences with deferred capture key', function () {
-// //
-// //         });
-// //     });
-// // })
+'use strict';
+
+// almaConfigHelper.js unit tests
+
+var assert = require('chai').assert;
+var site = require('../../../../mocks/helpers/AlmaConfigHelpers').site;
+var almaConfigHelpers = require('../../../../mocks/helpers/AlmaConfigHelpers').almaConfigHelpers;
+
+describe('Get back-office variables', function () {
+    describe('Deferred Capture', function () {
+        it('should call site preferences with deferred capture key', function () {
+            almaConfigHelpers.isDeferredCaptureEnable();
+
+            assert.isTrue(site.getCurrent().getCustomPreferenceValue.calledOnce);
+            assert.isTrue(site.getCurrent().getCustomPreferenceValue.calledWith('ALMA_Deferred_Capture_Activation'));
+        });
+        it('should call site preferences and return default value', function () {
+            assert.equal(false, almaConfigHelpers.isDeferredCaptureEnable());
+        });
+    });
+});
