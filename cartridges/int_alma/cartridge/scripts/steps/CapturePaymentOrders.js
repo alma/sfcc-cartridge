6bba0c7ada221f37a429c1bdaaa21a220e30321d
joyet-simon <simon.joyet@getalma.eu>
2023-10-12 16:40:54 +0200

    extract some function

--- cartridges/int_alma/cartridge/scripts/steps/CapturePaymentOrders.js
+++ cartridges/int_alma/cartridge/scripts/steps/CapturePaymentOrders.js
@@ -1,13 +1,41 @@
 'use strict';
 
-var OrderMgr = require('dw/order/OrderMgr');
 var Order = require('dw/order/Order');
 var Status = require('dw/system/Status');
 var almaOrderHelper = require('*/cartridge/scripts/helpers/almaOrderHelper');
 var almaPaymentHelper = require('*/cartridge/scripts/helpers/almaPaymentHelper');
-var Logger = require('dw/system/Logger');
+
+/**
+ * Try to make a capture while we have an order
+ * @param {Object} orders orders
+ * @param {Object} CAPTURE string constante for capture
+ */
+function makeCaptureWhileHaveAnOrder(orders, CAPTURE) {
+    var Logger = require('dw/system/Logger');
+    while (orders.hasNext()) {
+        var order = orders.next();
+        var params = { external_id: order.custom.almaPaymentId };
+        var amount = order.getTotalGrossPrice().value;
+        var captureType = CAPTURE.total;
+        var partialCaptureAmount = almaOrderHelper.getPartialCaptureAmount(order);
+        try {
+            if (partialCaptureAmount) {
+                captureType = CAPTURE.partial;
+                amount = partialCaptureAmount;
+            }
+            params.amount = amount * 100;
+            var capture = almaPaymentHelper.capturePayment(params);
+            almaOrderHelper.setAlmaDeferredCaptureFields(order, captureType.code, amount);
+            Logger.info(captureType.description + ' payment: order id: {0} - payment id: {1} - capture id : {2}', [order.orderNo, order.custom.almaPaymentId, capture.id]);
+        } catch (e) {
+            almaOrderHelper.setAlmaDeferredCaptureFields(order, CAPTURE.failed.code);
+            Logger.warn(CAPTURE.failed.description + ' payment: order id: {0}, payment id: {1}', [order.orderNo, order.custom.almaPaymentId]);
+        }
+    }
+}
 
 exports.execute = function () {
+    var OrderMgr = require('dw/order/OrderMgr');
     var CAPTURE = almaPaymentHelper.CAPTURE;
     var orders = OrderMgr.searchOrders(
         'custom.ALMA_Deferred_Capture_Status={0} and status != {1} and status != {2}',
@@ -18,26 +46,7 @@ exports.execute = function () {
     );
 
     if (orders.count > 0) {
-        while (orders.hasNext()) {
-            var order = orders.next();
-            var params = { external_id: order.custom.almaPaymentId };
-            var amount = order.getTotalGrossPrice().value;
-            var captureType = CAPTURE.total;
-            var partialCaptureAmount = almaOrderHelper.getPartialCaptureAmount(order);
-            try {
-                if (partialCaptureAmount) {
-                    captureType = CAPTURE.partial;
-                    amount = partialCaptureAmount;
-                }
-                params.amount = amount * 100;
-                var capture = almaPaymentHelper.capturePayment(params);
-                almaOrderHelper.setAlmaDeferredCaptureFields(order, captureType.code, amount);
-                Logger.info(captureType.description + ' payment: order id: {0} - payment id: {1} - capture id : {2}', [order.orderNo, order.custom.almaPaymentId, capture.id]);
-            } catch (e) {
-                almaOrderHelper.setAlmaDeferredCaptureFields(order, CAPTURE.failed.code);
-                Logger.warn(CAPTURE.failed.description + ' payment: order id: {0}, payment id: {1}', [order.orderNo, order.custom.almaPaymentId]);
-            }
-        }
+        makeCaptureWhileHaveAnOrder(orders, CAPTURE);
     }
 
     return new Status(Status.OK);
